26ccdbb5b40e59ffb052063bfc5258b3b666b6be
softwareguycoder <softwareguycoder@gmail.com>
2019-04-11 22:50:17 -0400

    Fixing formatting

--- inetsock_core/src/utils.c
+++ inetsock_core/src/utils.c
@@ -1,16 +1,15 @@
 ///////////////////////////////////////////////////////////////////////////////
-// SocketDemoUtils.c: Definitions for the functions in the SocketDemoUtils.lib
+// utils.c: Definitions for the functions in the SocketDemoUtils.lib
 // shared library
 
-#include <inetsock_core.h>
 #include "stdafx.h"
 #include "utils.h"
 
 /**
- * \brief Checks the integer value supplied to ensure it's a valid user port
+ * @brief Checks the integer value supplied to ensure it's a valid user port
  * number and not reserved for a different service.
- * \param nPort Value to be validated.
- * \returns Zero if the 'port' parameter is not in the range [1024, 49151]
+ * @param nPort Value to be validated.
+ * @returns Zero if the 'port' parameter is not in the range [1024, 49151]
  * (inclusive); nonzero otherwise.
  */
 int IsUserPortValid(int nPort)
