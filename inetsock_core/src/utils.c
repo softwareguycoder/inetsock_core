9b43235cb7d68b2db7be33a920c4796520e60dca
softwareguycoder <softwareguycoder@gmail.com>
2019-04-14 16:04:30 -0400

    Removed non-needed files

--- inetsock_core/src/utils.c
+++
@@ -1,9 +0,0 @@
-///////////////////////////////////////////////////////////////////////////////
-// utils.c: Definitions for the functions in the SocketDemoUtils.lib
-// shared library
-
-#include "stdafx.h"
-#include "utils.h"
-
-
-
